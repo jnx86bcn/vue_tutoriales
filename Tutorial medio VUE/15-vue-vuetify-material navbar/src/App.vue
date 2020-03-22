593946caa4276a6134a3b15989899d3acccdec73
jnx86bcn <jnx86bcn@gmail.com>
2020-03-22 22:57:24 +0100

    navigation bar

--- Tutorial medio VUE/15-vue-vuetify-material navbar/src/App.vue
+++ Tutorial medio VUE/15-vue-vuetify-material navbar/src/App.vue
@@ -1,34 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <v-card color="grey lighten-4" flat tile>
+      <v-toolbar>
+        <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Vuetify</v-toolbar-title>
+      </v-toolbar>
+    </v-card>
+    <v-navigation-drawer v-model="drawer" temporary app>
+      <v-list dense nav>
+        <v-list-item link :to="{name: 'Home'}" exact>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link :to="{name: 'Grid'}">
+          <v-list-item-content>
+            <v-list-item-title>Grid</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link :to="{name: 'Grid2'}">
+          <v-list-item-content>
+            <v-list-item-title>Grid2</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link :to="{name: 'Buttons'}">
+          <v-list-item-content>
+            <v-list-item-title>Buttons</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <router-view />
     </v-content>
@@ -37,6 +38,11 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      drawer: false
+    };
+  }
 };
 </script>

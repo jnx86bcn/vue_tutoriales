8c0a6fabc8e94c618063352ea9ed4499354acc02
jnx86bcn <jnx86bcn@gmail.com>
2020-03-22 13:50:48 +0100

    clean vue files

--- Tutorial medio VUE/11-vue-axios-vuetify/src/main.js
+++ Tutorial medio VUE/11-vue-axios-vuetify/src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: h => h(App)
+}).$mount('#app');

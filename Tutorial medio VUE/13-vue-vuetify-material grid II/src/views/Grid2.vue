e90fd7d86931ba3d5998fc63af9a4f564db3a8e1
jnx86bcn <jnx86bcn@gmail.com>
2020-03-22 21:42:42 +0100

    new folder buttons

---
+++ Tutorial medio VUE/13-vue-vuetify-material grid II/src/views/Grid2.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <h1>Grid example 2</h1>
+    <v-layout wrap mt-5>
+      <v-flex class="primary">
+        <h3>12</h3>
+      </v-flex>
+    </v-layout>
+    <v-layout wrap mt-5>
+      <v-flex class="primary" xs6>
+        <h3>6</h3>
+      </v-flex>
+      <v-flex class="secondary" xs6>
+        <h3>6</h3>
+      </v-flex>
+    </v-layout>
+    <v-layout wrap mt-5>
+      <v-flex class="primary" sm4>
+        <h3>4</h3>
+      </v-flex>
+      <v-flex class="secondary" sm8>
+        <h3>8</h3>
+      </v-flex>
+    </v-layout>
+    <v-layout wrap mt-5 class="secondary" justify-center align-center style="height:100px">
+      <v-flex class="primary" xs3>
+        <h3>3</h3>
+      </v-flex>
+      <v-flex class="primary" xs3>
+        <h3>3</h3>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>
+        <h1 class="display-1 text-uppercase">Lorem, ipsum dolor.</h1>
+        <h1 class="display-2 primary pa-5 mt-5">Lorem, ipsum dolor.</h1>
+        <h1 class="display-3 text-xs-center">Lorem, ipsum dolor.</h1>
+        <h1 class="display-4">Lorem, ipsum dolor.</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
---
+++ Tutorial medio VUE/14-vue-vuetify-material buttons/src/views/Buttons.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container>
+    <h1>Buttons examples</h1>
+    <v-row></v-row>
+  </v-container>
+</template>
